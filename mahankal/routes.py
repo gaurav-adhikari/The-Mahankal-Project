9610f48f8034f918e85cc562c735b7f4a68c7901
gaurav <gaurav.adhikari36@gmail.com>
2020-08-12 21:06:47 +0545

    login module initialised

--- mahankal/routes.py
+++ mahankal/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, url_for
 from mahankal import app
+from mahankal.forms import LoginForm
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('home.html')
+    form=LoginForm()
+    return render_template('home.html',form=form)
+
+
